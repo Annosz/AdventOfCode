65e280b91f76af32653748dd9245a324b7cbb4dd
Adam Toth <adam_toth4@epam.com>
2022-12-06 08:38:47 +0100

    Add solution for Day 6

---
+++ 2022/Day6.cs
@@ -0,0 +1,19 @@
+﻿namespace _2022;
+
+public static class Day6
+{
+    public static string Solve()
+    {
+        int index;
+        var line = File.ReadAllText(@".\Input\Day6.txt");
+
+        for (index = 14; index < line.Length; index++)
+        {
+            var asd = line[(index - 14)..index];
+            if (line[(index - 14)..index].ToHashSet().Count == 14)
+                break;
+        }
+
+        return index.ToString();
+    }
+}
