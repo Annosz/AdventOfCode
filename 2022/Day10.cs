5e0cc9e5f811cdcf748a8f9409e6d08f315f1c44
Adam Toth <adam_toth4@epam.com>
2022-12-10 07:48:58 +0100

    Add solution for Day 10

---
+++ 2022/Day10.cs
@@ -0,0 +1,49 @@
+﻿namespace _2022;
+
+public static class Day10
+{
+    public static string Solve()
+    {
+        List<int> timeline = new List<int> { 1 };
+
+        int t = 1;
+        foreach (string line in File.ReadLines(@".\Input\Day10.txt"))
+        {
+            if (timeline.Count <= t)
+            {
+                timeline.Add(timeline[t - 1]);
+            }
+
+            if (line == "noop")
+            {
+                t++;
+                continue;
+            }
+
+            timeline.Add(timeline[t]);
+            timeline.Add(timeline[t] + int.Parse(line.Split(" ")[1]));
+            t += 2;
+        }
+
+        int signalStrength = 0;
+        for (int i = 20; i <= timeline.Count; i += 40)
+        {
+            signalStrength += i * timeline[i];
+        }
+
+        for (int i = 1; i <= 240; i++)
+        {
+            int drawnPixel = i % 40 - 1;
+            int[] spite = { timeline[i] - 1, timeline[i], timeline[i] + 1 };
+            Console.Write(spite.Contains(drawnPixel) ? "#" : ".");
+
+            if (i % 40 == 0)
+            {
+                Console.WriteLine();
+            }
+        }
+
+        Console.WriteLine();
+        return signalStrength.ToString();
+    }
+}
--- 2022/Program.cs
+++ 2022/Program.cs
@@ -1,3 +1,3 @@
 ﻿using _2022;
 
-Console.WriteLine(Day9.Solve());
+Console.WriteLine(Day10.Solve());
