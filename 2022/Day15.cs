b8544fcb43d77ddb5ce36827c02484084005f511
Adam Toth <adam_toth4@epam.com>
2022-12-15 17:18:50 +0100

    Add solution for Day 15 part 1

--- 2022/Day15.cs
+++ 2022/Day15.cs
@@ -2,96 +2,73 @@
 
 public static class Day15
 {
+    private const int LineNumber = 2000000;
+
+    private static readonly HashSet<Coordinate> FilledCoordinates = new();
+    private static readonly Dictionary<Coordinate, char> SymbolMap = new();
+
     public static string Solve()
     {
-        List<List<char>> forbiddenMap = new List<List<char>>();
-        List<(Coordinate, int)> sensors = new List<(Coordinate, int)>();
-        HashSet<Coordinate> beacons = new HashSet<Coordinate>();
+        List<(Coordinate, int)> sensors = new();
+        HashSet<Coordinate> beacons = new();
 
-        int smallestX = int.MaxValue;
-        int smallestY = int.MaxValue;
-        int largestX = int.MinValue;
-        int largestY = int.MinValue;
-        int largestManhattan = int.MinValue;
         foreach (var line in File.ReadLines(@".\Input\Day15.txt"))
         {
-            var lineWorkingCopy = line.Remove(0, "Sensor at x=".Length);
-            var lineSplit = lineWorkingCopy.Split(',', 2);
-            var sensorX = int.Parse(lineSplit[0]);
-            smallestX = sensorX < smallestX ? sensorX : smallestX;
-            largestX = sensorX > largestX ? sensorX : largestX;
+            string lineWorkingCopy = line.Remove(0, "Sensor at x=".Length);
+            string[] lineSplit = lineWorkingCopy.Split(',', 2);
+            int sensorX = int.Parse(lineSplit[0]);
             lineWorkingCopy = lineSplit[1];
 
             lineWorkingCopy = lineWorkingCopy.Remove(0, " y=".Length);
             lineSplit = lineWorkingCopy.Split(':', 2);
-            var sensorY = int.Parse(lineSplit[0]);
-            smallestY = sensorY < smallestY ? sensorY : smallestY;
-            largestY = sensorY > largestY ? sensorY : largestY;
+            int sensorY = int.Parse(lineSplit[0]);
             lineWorkingCopy = lineSplit[1];
 
 
             lineWorkingCopy = lineWorkingCopy.Remove(0, " closest beacon is at x=".Length);
             lineSplit = lineWorkingCopy.Split(',', 2);
-            var beaconX = int.Parse(lineSplit[0]);
+            int beaconX = int.Parse(lineSplit[0]);
             lineWorkingCopy = lineSplit[1];
 
             lineWorkingCopy = lineWorkingCopy.Remove(0, " y=".Length);
-            var beaconY = int.Parse(lineWorkingCopy);
+            int beaconY = int.Parse(lineWorkingCopy);
 
-            var manhattanDistance = Math.Abs(sensorX - beaconX) + Math.Abs(sensorY - beaconY);
-            largestManhattan = largestManhattan < manhattanDistance ? manhattanDistance : largestManhattan;
-            sensors.Add((new(sensorX, sensorY), manhattanDistance));
-            beacons.Add(new(beaconX, beaconY));
-        }
-
-        int offset = Math.Max(Math.Max(Math.Abs(smallestX - largestManhattan), Math.Abs(smallestY - largestManhattan)), largestManhattan);
-
-        for (int i = 0; i < largestX + 2 * offset + 1; i++)
-        {
-            forbiddenMap.Add(Enumerable.Repeat('.', largestY + 2 * offset + 1).ToList());
+            int manhattanDistance = Math.Abs(sensorX - beaconX) + Math.Abs(sensorY - beaconY);
+            sensors.Add((new Coordinate(sensorX, sensorY), manhattanDistance));
+            beacons.Add(new Coordinate(beaconX, beaconY));
         }
 
         foreach (var coordinate in beacons)
         {
-            forbiddenMap[coordinate.X + offset][coordinate.Y + offset] = 'B';
+            FillOnMap(coordinate, 'B');
         }
 
-        foreach (var (coordinate, manhattan) in sensors)
+        foreach (var (coordinate, _) in sensors)
         {
-            forbiddenMap[coordinate.X + offset][coordinate.Y + offset] = 'S';
+            FillOnMap(coordinate, 'S');
+        }
 
+        foreach ((var coordinate, int manhattan) in sensors)
+        {
             for (int i = coordinate.X - manhattan; i <= coordinate.X + manhattan; i++)
             {
-                for (int j = coordinate.Y - manhattan; j <= coordinate.Y + manhattan; j++)
+                if (Math.Abs(coordinate.X - i) + Math.Abs(coordinate.Y - LineNumber) <= manhattan)
                 {
-                    if (forbiddenMap[i + offset][j + offset] == '.'
-                        && Math.Abs(coordinate.X - i) + Math.Abs(coordinate.Y - j) <= manhattan)
-                    {
-                        forbiddenMap[i + offset][j + offset] = '#';
-                    }
+                    FillOnMap(new Coordinate(i, LineNumber), '#');
                 }
             }
         }
 
-        Visualize(forbiddenMap);
-
-        Console.WriteLine();
-        Console.WriteLine(forbiddenMap.Select(l => l[10 + offset]).ToArray());
-
-        return forbiddenMap.Select(l => l[10 + offset]).Count(c => c == '#').ToString();
+        return FilledCoordinates.Count(c => c.Y == 2000000 && SymbolMap.ContainsKey(c) && SymbolMap[c] == '#').ToString();
     }
 
-    private static void Visualize(List<List<char>> forbiddenMap)
+    private static void FillOnMap(Coordinate coordinate, char symbol)
     {
-        for (int j = 0; j < forbiddenMap[0].Count; j++)
-        {
-            for (int i = 0; i < forbiddenMap.Count; i++)
-            {
-                Console.Write(forbiddenMap[i][j]);
-            }
+        if (SymbolMap.ContainsKey(coordinate))
+            return;
 
-            Console.WriteLine();
-        }
+        FilledCoordinates.Add(coordinate);
+        SymbolMap.Add(coordinate, symbol);
     }
 
     private record Coordinate(int X, int Y);
