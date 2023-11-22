2dad0dbbc16c94cce6053c45d76feed10896638f
Adam Toth <tadamleanyfalu@gmail.com>
2023-11-22 13:53:51 +0100

    Optimize Day 16 solutions

--- 2022/Day16.cs
+++ 2022/Day16.cs
@@ -1,6 +1,4 @@
-﻿using System.Globalization;
-using System.Linq;
-using System.Runtime.CompilerServices;
+﻿using Shared;
 
 namespace _2022;
 
@@ -39,7 +37,7 @@ public static class Day16
             edges.AddRange(lineSplit.Select(v => new Edge(node, v, 1)));
         }
 
-        // Reduce graph
+        // Reduce graph to value nodes
         var zeroValueNodes = nodeValues.Where(nv => nv.Value == 0).Select(nv => nv.Key).ToList();
         zeroValueNodes.Remove(starterNode);
         foreach (var zeroValueNode in zeroValueNodes)
@@ -62,20 +60,39 @@ public static class Day16
             nodeValues.Remove(zeroValueNode);
         }
 
-        // Initialize 0 values as already collected
-        HashSet<string> collectedValues = nodeValues.Where(nv => nv.Value == 0).Select(nv => nv.Key).ToHashSet();
-        SearchForBestSolutionFromMinute(0, 1, "", starterNode, collectedValues, new List<string>());
+        // Create complete graph
+        List<Edge> newlyAddedEdges = new List<Edge>();
+        do
+        {
+            newlyAddedEdges = new List<Edge>();
 
-        foreach(string line in maxTrail)
+            foreach (var firstEdge in edges)
+            {
+                foreach (var secondEdge in edges)
+                {
+                    if (firstEdge.U != secondEdge.V && firstEdge.V == secondEdge.U && !edges.Any(e => e.U == firstEdge.U && e.V == secondEdge.V))
+                    {
+                        newlyAddedEdges.Add(new Edge(firstEdge.U, secondEdge.V, firstEdge.Weight + secondEdge.Weight));
+                    }
+                }
+            }
+
+            edges.AddRange(newlyAddedEdges);
+
+        } while (newlyAddedEdges.Count > 0);
+
+        //Initialize 0 values as already collected
+        HashSet<string> collectedValues = nodeValues.Where(nv => nv.Value == 0).Select(nv => nv.Key).ToHashSet();
+        SearchForBestSolutionFromMinute(0, 0, starterNode, collectedValues, new List<string>());
+
+        foreach (string line in maxTrail)
             Console.WriteLine(line);
 
         return maxValvePressure.ToString();
     }
-
     public static void SearchForBestSolutionFromMinute(
         int currentValue, 
         int minute,
-        string previousNode,
         string currentNode,
         HashSet<string> collectedValues,
         List<string> trail)
@@ -83,31 +100,40 @@ public static class Day16
         if (minute > 30 || collectedValues.Count == nodeValues.Count)
             return;
 
-        if (!collectedValues.Contains(currentNode))
+        if (collectedValues.Contains(currentNode))
         {
-            trail.Add($"Minute {minute}; Open valve {currentNode}, adding a {nodeValues[currentNode]} flow for {30 - minute} minutes (total {nodeValues[currentNode] * (30 - minute)})");
-            collectedValues.Add(currentNode);
+            GoToNextValve(currentValue, minute, currentNode, collectedValues, trail);
+        }
+        else
+        {
+            TurnOnValve(currentValue, minute, currentNode, collectedValues, trail);
+        }
+    }
 
-            int flowUntilEnd = nodeValues[currentNode] * (30 - minute);
-            if (currentValue + flowUntilEnd > maxValvePressure)
-            {
-                maxTrail = new List<string>(trail);
-                maxValvePressure = currentValue + flowUntilEnd;
-            }
+    private static void TurnOnValve(int currentValue, int minute, string currentNode, HashSet<string> collectedValues, List<string> trail)
+    {
+        trail.Add($"Minute {minute}; Open valve {currentNode}, adding a {nodeValues[currentNode]} flow for {30 - minute} minutes (total {nodeValues[currentNode] * (30 - minute)})");
+        collectedValues.Add(currentNode);
 
-            SearchForBestSolutionFromMinute(currentValue + flowUntilEnd, minute + 1, "", currentNode, collectedValues, trail);
-
-            trail.RemoveAt(trail.Count - 1);
-            collectedValues.Remove(currentNode);
+        int flowUntilEnd = nodeValues[currentNode] * (30 - minute - 1);
+        if (currentValue + flowUntilEnd > maxValvePressure)
+        {
+            maxTrail = new List<string>(trail);
+            maxValvePressure = currentValue + flowUntilEnd;
         }
 
-        foreach (var nextEdge in edges.Where(e => e.U == currentNode && e.V != previousNode))
-        {
-            if (collectedValues.Contains(nextEdge.V) && edges.Count(e => e.U == nextEdge.V) == 1)
-                continue;
+        SearchForBestSolutionFromMinute(currentValue + flowUntilEnd, minute + 1, currentNode, collectedValues, trail);
 
+        collectedValues.Remove(currentNode);
+        trail.RemoveAt(trail.Count - 1);
+    }
+
+    private static void GoToNextValve(int currentValue, int minute, string currentNode, HashSet<string> collectedValues, List<string> trail)
+    {
+        foreach (var nextEdge in edges.Where(e => e.U == currentNode && !collectedValues.Contains(e.V)))
+        {
             trail.Add($"Minute {minute}; Go to valve {nextEdge.V}");
-            SearchForBestSolutionFromMinute(currentValue, minute + nextEdge.Weight, currentNode, nextEdge.V, collectedValues, trail);
+            SearchForBestSolutionFromMinute(currentValue, minute + nextEdge.Weight, nextEdge.V, collectedValues, trail);
             trail.RemoveAt(trail.Count - 1);
         }
     }
