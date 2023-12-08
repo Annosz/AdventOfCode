994296fafe9a2fd111a37d6c249972fe96f7acb9
Adam Toth <tadamleanyfalu@gmail.com>
2023-12-08 11:44:21 +0100

    Add solution to Day 8 part 2

--- 2023/Day8.cs
+++ 2023/Day8.cs
@@ -1,9 +1,16 @@
-﻿namespace _2023;
+﻿using Shared;
+
+namespace _2023;
 
 public static class Day8
 {
     private static string Directions = "";
-    private static Dictionary<string, string[]> Map = new Dictionary<string, string[]>();
+    private static readonly Dictionary<string, string[]> Map = new();
+
+    private const string part1Start = "AAA";
+    private const string part1End = "ZZZ";
+    private const string part2Start = "A";
+    private const string part2End = "Z";
 
     public static string Solve()
     {
@@ -17,6 +24,7 @@ public static class Day8
                 Directions = new string(line);
                 continue;
             }
+
             var lineSplit = line.Split(" = ", StringSplitOptions.RemoveEmptyEntries);
             var node = lineSplit[0];
             var edges = lineSplit[1].Replace("(", "").Replace(")", "").Split(", ");
@@ -24,20 +32,45 @@ public static class Day8
             Map.Add(node, edges);
         }
 
-        long stepCount = 0;
+        int stepCount = 0;
         int directionPointer = 0;
-        string[] currentNodes = Map.Keys.Where(n => n.EndsWith("AAA")).ToArray();
-        while (!currentNodes.All(n => n.EndsWith("ZZZ")))
+        string[] currentNodes = Map.Keys.Where(n => n.EndsWith(part2Start)).ToArray();
+        Dictionary<int, List<int>> zStepTimes = new();
+        for (int i = 0; i < currentNodes.Length; i++)
+        {
+            zStepTimes.Add(i, new List<int>());
+        }
+
+        // Gather some data, constant patterns should emerge by 10 iterations
+        while (!zStepTimes.Values.All(z => z.Count >= 10))
         {
             for (int i = 0; i < currentNodes.Length; i++)
             {
                 currentNodes[i] = Map[currentNodes[i]][Directions[directionPointer] == 'L' ? 0 : 1];
+                if (currentNodes[i].EndsWith(part2End))
+                {
+                    zStepTimes[i].Add(stepCount);
+                }
             }
             stepCount++;
             directionPointer++;
             directionPointer %= Directions.Length;
         }
 
-        return stepCount.ToString();
+        // Find the intervals between arriving at Z
+        Dictionary<int, List<int>> zStepTimeDifferences = new();
+        for (int i = 0; i < currentNodes.Length; i++)
+        {
+            zStepTimeDifferences.Add(i, new List<int>());
+            zStepTimeDifferences[i].Add(zStepTimes[i].First());
+            for (int j = 1; j < zStepTimes[i].Count; j++)
+            {
+                zStepTimeDifferences[i].Add(zStepTimes[i][j] - zStepTimes[i][j - 1]);
+            }
+        }
+
+        return MathHelpers.LeastCommonMultiple(zStepTimeDifferences.Values.Select(z => z.Last()).ToArray()).ToString();
     }
+
+
 }
---
+++ Shared/MathHelpers.cs
@@ -0,0 +1,65 @@
+﻿namespace Shared;
+public static class MathHelpers
+{
+    public static long LeastCommonMultiple(int[] elements)
+    {
+        long leastCommonMultiple = 1;
+        int divisor = 2;
+
+        while (true)
+        {
+
+            int counter = 0;
+            bool divisible = false;
+            for (int i = 0; i < elements.Length; i++)
+            {
+
+                // LeastCommonMultiple (n1, n2, ... 0) = 0.
+                // For negative number we convert into
+                // positive and calculate lcm_of_array_elements.
+                if (elements[i] == 0)
+                {
+                    return 0;
+                }
+                else if (elements[i] < 0)
+                {
+                    elements[i] *= -1;
+                }
+                if (elements[i] == 1)
+                {
+                    counter++;
+                }
+
+                // Divide elements by devisor if complete
+                // division i.e. without remainder then replace
+                // number with quotient; used for find next factor
+                if (elements[i] % divisor == 0)
+                {
+                    divisible = true;
+                    elements[i] /= divisor;
+                }
+            }
+
+            // If divisor able to completely divide any number
+            // from array multiply with LeastCommonMultiple
+            // and store into LeastCommonMultiple and continue
+            // to same divisor for next factor finding.
+            // else increment divisor
+            if (divisible)
+            {
+                leastCommonMultiple *= divisor;
+            }
+            else
+            {
+                divisor++;
+            }
+
+            // Check if all element_array is 1 indicate 
+            // we found all factors and terminate while loop.
+            if (counter == elements.Length)
+            {
+                return leastCommonMultiple;
+            }
+        }
+    }
+}
