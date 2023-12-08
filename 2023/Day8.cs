7c2ee94c7452ff6b8b9828e2b72ff26db7a40201
Adam Toth <tadamleanyfalu@gmail.com>
2023-12-08 08:39:51 +0100

    Add solution to Day 8 part 1

---
+++ 2023/Day8.cs
@@ -0,0 +1,43 @@
+﻿namespace _2023;
+
+public static class Day8
+{
+    private static string Directions = "";
+    private static Dictionary<string, string[]> Map = new Dictionary<string, string[]>();
+
+    public static string Solve()
+    {
+        foreach (var line in File.ReadLines(@".\Input\Day8.txt"))
+        {
+            if (string.IsNullOrEmpty(line))
+                continue;
+
+            if (!line.Contains(" = "))
+            {
+                Directions = new string(line);
+                continue;
+            }
+            var lineSplit = line.Split(" = ", StringSplitOptions.RemoveEmptyEntries);
+            var node = lineSplit[0];
+            var edges = lineSplit[1].Replace("(", "").Replace(")", "").Split(", ");
+
+            Map.Add(node, edges);
+        }
+
+        long stepCount = 0;
+        int directionPointer = 0;
+        string[] currentNodes = Map.Keys.Where(n => n.EndsWith("AAA")).ToArray();
+        while (!currentNodes.All(n => n.EndsWith("ZZZ")))
+        {
+            for (int i = 0; i < currentNodes.Length; i++)
+            {
+                currentNodes[i] = Map[currentNodes[i]][Directions[directionPointer] == 'L' ? 0 : 1];
+            }
+            stepCount++;
+            directionPointer++;
+            directionPointer %= Directions.Length;
+        }
+
+        return stepCount.ToString();
+    }
+}
--- 2023/Program.cs
+++ 2023/Program.cs
@@ -1,3 +1,3 @@
 ﻿using _2023;
 
-Console.WriteLine(Day7.Solve());
+Console.WriteLine(Day8.Solve());
