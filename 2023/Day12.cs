997fd0f26b62a862eae11637b5635d9f778fbab3
Adam Toth <tadamleanyfalu@gmail.com>
2023-12-12 15:48:02 +0100

    Add solution for Day 12 part 2

--- 2023/Day12.cs
+++ 2023/Day12.cs
@@ -2,7 +2,9 @@
 
 public static class Day12
 {
-    private static List<(string, int[])> Map = new();
+    private static readonly List<(string, int[])> Map = new();
+
+    // 1 for Part 1, 5 for Part 2
     private const int RepeatFactor = 5;
 
     public static string Solve()
@@ -13,57 +15,50 @@ public static class Day12
             var lineSplitBegin = string.Join("?", Enumerable.Repeat(lineSplit[0], RepeatFactor));
             var lineSplitEnd = string.Join(",", Enumerable.Repeat(lineSplit[1], RepeatFactor));
             Map.Add((lineSplitBegin, lineSplitEnd.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).ToArray()));
+
+            var template = string.Join("?", Enumerable.Repeat(lineSplit[0], RepeatFactor));
+            var intervals = string.Join(",", Enumerable.Repeat(lineSplit[1], RepeatFactor)).Split(',', StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).ToArray();
+            Map.Add((template, intervals));
         }
 
         long allPossibilities = 0;
-        int count = 1;
-        foreach (var (mapLine, intervals) in Map)
+        foreach (var (template, intervals) in Map)
         {
-            Console.WriteLine($"{count}: {mapLine}");
-            GenerateAllPossibilities(mapLine, intervals, ref allPossibilities);
-            count++;
+            long[,] solutionSpace = new long[template.Length + 1, intervals.Length + 1];
+
+            for (int templateLength = 0; templateLength <= template.Length; templateLength++)
+            {
+                for (int intervalCount = 0; intervalCount <= intervals.Length; intervalCount++)
+                {
+                    string currentTempalte = template[..templateLength];
+
+                    if (intervalCount == 0)
+                    {
+                        solutionSpace[templateLength, intervalCount] = !currentTempalte.Any(e => e == '#') ? 1 : 0;
+                        continue;
+                    }
+
+                    if (templateLength == 0)
+                    {
+                        solutionSpace[templateLength, intervalCount] = 0;
+                        continue;
+                    }
+
+                    int currentInterval = intervals[intervalCount - 1];
+
+                    bool mustFitToEnd = currentTempalte.Last() == '#';
+                    bool canFitToEnd = templateLength >= currentInterval
+                        && !currentTempalte[^currentInterval..].Any(e => e == '.')
+                        && (templateLength == currentInterval || currentTempalte[^(currentInterval + 1)] != '#');
+                    solutionSpace[templateLength, intervalCount] =
+                        (mustFitToEnd ? 0 : solutionSpace[templateLength - 1, intervalCount])
+                        + (canFitToEnd ? (solutionSpace[Math.Max(templateLength - currentInterval - 1, 0), intervalCount - 1]) : 0);
+                }
+            }
+
+            allPossibilities += solutionSpace[template.Length, intervals.Length];
         }
 
         return allPossibilities.ToString();
-    }
-
-    private static void GenerateAllPossibilities(string mapLine, int[] intervals, ref long allPossibilities)
-    {
-        if (!mapLine.Any() && intervals.Any())
-            return;
-
-        if (!intervals.Any())
-        {
-            if (!mapLine.Contains('#'))
-                allPossibilities++;
-            return;
-        }
-
-        if (mapLine.Length < (intervals.Sum() + intervals.Count() - 1))
-            return;
-
-        var segmentLengths = mapLine.Split('.', StringSplitOptions.RemoveEmptyEntries).Select(e => e.Length);
-        if (segmentLengths.Sum() < intervals.Sum())
-            return;
-
-        int longestInterval = intervals.Max();
-        if (segmentLengths.Sum(l => Math.Floor(l / (decimal)longestInterval)) < intervals.Where(e => e == longestInterval).Count())
-            return;
-
-        switch (mapLine.First())
-        {
-            case '.':
-                GenerateAllPossibilities(mapLine.Remove(0, 1), intervals, ref allPossibilities);
-                break;
-            case '?':
-                GenerateAllPossibilities(mapLine.Remove(0, 1), intervals, ref allPossibilities);
-                if (mapLine.Length >= intervals[0] && !mapLine.Substring(0, intervals[0]).Contains('.') && (mapLine.Length == intervals[0] || mapLine[intervals[0]] != '#'))
-                    GenerateAllPossibilities(mapLine.Remove(0, Math.Min(mapLine.Length, intervals[0] + 1)), intervals[1..], ref allPossibilities);
-                break;
-            case '#':
-                if (mapLine.Length >= intervals[0] && !mapLine.Substring(0, intervals[0]).Contains('.') && (mapLine.Length == intervals[0] || mapLine[intervals[0]] != '#'))
-                    GenerateAllPossibilities(mapLine.Remove(0, Math.Min(mapLine.Length, intervals[0] + 1)), intervals[1..], ref allPossibilities);
-                break;
-        }
     }
 }
