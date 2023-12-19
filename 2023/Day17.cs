574a2e3c9514a3cf62458c1b14743633c83667a2
Adam Toth <tadamleanyfalu@gmail.com>
2023-12-19 15:41:16 +0100

    Imprive Day 17 solutions

--- 2023/Day17.cs
+++ 2023/Day17.cs
@@ -6,30 +6,32 @@ public static class Day17
     private const int MaxDistance = 10;
 
     public static List<Direction> Directions = new() { new(1, 0), new(0, 1), new(-1, 0), new(0, -1) };
-    public static Dictionary<Direction, int> DirectionAxis = new() { { new(1, 0), 0 }, { new(0, 1), 1 }, { new(-1, 0), 0 }, { new(0, -1), 1 } };
+    public static Dictionary<Direction, int> DirectionAxis = new() { { new(1, 0), 0 }, { new(0, 1), 1 }, { new(-1, 0), 2 }, { new(0, -1), 3 } };
     public static Dictionary<Direction, string> DirectionString = new() { { new(1, 0), "↓" }, { new(0, 1), "→" }, { new(-1, 0), "↑" }, { new(0, -1), "←" } };
 
     private static readonly List<List<int>> Map = new();
-    private static readonly List<List<int>>[][] Distances = new List<List<int>>[][] { new List<List<int>>[MaxDistance], new List<List<int>>[MaxDistance] };
+    private static readonly List<List<int>>[][] Distances = new List<List<int>>[][] {
+        new List<List<int>>[MaxDistance], new List<List<int>>[MaxDistance], new List<List<int>>[MaxDistance], new List<List<int>>[MaxDistance]
+    };
 
     public static string Solve()
     {
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < 4; i++)
             for (int j = 0; j < MaxDistance; j++)
                 Distances[i][j] = new();
 
         foreach (var line in File.ReadLines(@".\Input\Day17.txt"))
         {
             Map.Add(line.Select(c => int.Parse(c.ToString())).ToList());
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 4; i++)
                 for (int j = 0; j < MaxDistance; j++)
                     Distances[i][j].Add(Enumerable.Repeat(-1, line.Length).ToList());
         }
 
         PriorityQueue<Point, int> priorityQueue = new();
-        Distances[0][0][1][0] = Map[1][0];
+        Distances[DirectionAxis[new(1, 0)]][0][1][0] = Map[1][0];
         priorityQueue.Enqueue(new Point(1, 0, 0, new(1, 0), DirectionString[new(1, 0)]), Map[1][0]);
-        Distances[1][0][0][1] = Map[0][1];
+        Distances[DirectionAxis[new(0, 1)]][0][0][1] = Map[0][1];
         priorityQueue.Enqueue(new Point(0, 1, 0, new(0, 1), DirectionString[new(0, 1)]), Map[0][1]);
 
         while (priorityQueue.TryDequeue(out Point point, out int distance))
