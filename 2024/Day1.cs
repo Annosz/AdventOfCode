0aa1a31d8ab1455b57b77b1fe26d6f853aac0612
Adam Toth <adatoth@diligent.com>
2024-12-03 07:29:08 +0100

    Add solution for Day 1 & 2

---
+++ 2024/Day1.cs
@@ -0,0 +1,26 @@
+﻿namespace _2024;
+
+public static class Day1
+{
+    public static string Solve()
+    {
+        List<int> listA = [];
+        List<int> listB = [];
+
+        foreach (var line in File.ReadLines(@".\Input\Day1.txt"))
+        {
+            var numbers = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            listA.Add(int.Parse(numbers[0]));
+            listB.Add(int.Parse(numbers[1]));
+        }
+
+        listA.Sort();
+        listB.Sort();
+
+        var part1Sum = listA.Zip(listB, (a, b) => Math.Abs(a - b)).Sum();
+
+        long part2Sum = listA.Aggregate<int, long>(0, (current, num) => current + listB.Count(b => b == num) * num);
+
+        return part2Sum.ToString();
+    }
+}
---
+++ 2024/Day2.cs
@@ -0,0 +1,40 @@
+﻿namespace _2024;
+
+public static class Day2
+{
+    public static string Solve()
+    {
+        var correctReports = 0;
+
+        foreach (var line in File.ReadLines(@".\Input\Day2.txt"))
+        {
+            var rawReport = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            var dampenedReport = rawReport.Select((_, i) =>
+            {
+                var reportCopy = new List<int>(rawReport);
+                reportCopy.RemoveAt(i);
+                return reportCopy;
+            }).ToList();
+
+            var isAnyCorrect = dampenedReport.Any(report =>
+            {
+                var differences = report.Zip(report.Skip(1), (x, y) => y - x).ToList();
+                return Math.Min(NegativeDifferenceErrors(differences), PositiveDifferenceErrors(differences)) +
+                    SmallDifferenceErrors(differences) + LargeDifferenceErrors(differences) == 0;
+            });
+
+            correctReports += isAnyCorrect ? 1 : 0;
+        }
+
+        return correctReports.ToString();
+    }
+
+    private static int LargeDifferenceErrors(List<int> differences) => differences.Count(e => Math.Abs(e) > 3);
+
+    private static int SmallDifferenceErrors(List<int> differences) => differences.Count(e => Math.Abs(e) < 1);
+
+    private static int PositiveDifferenceErrors(List<int> differences) => Math.Abs(differences.Count(e => e > 0) - differences.Count);
+
+    private static int NegativeDifferenceErrors(List<int> differences) => Math.Abs(differences.Count(e => e < 0) - differences.Count);
+}
