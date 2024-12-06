5d6633d108d0acad206f9b88c52ad88ed5b01831
Annosz <tadamleanyfalu@gmail.com>
2024-12-06 08:30:40 +0100

    Clean the code

--- 2024/Day6.cs
+++ 2024/Day6.cs
@@ -9,7 +9,7 @@ public static class Day6
 
     private static List<List<char>> map = [];
     private static (int, int) position;
-    private static List<List<bool>> visited = [];
+    private static HashSet<(int, int)> visited = new();
     private static int direction = 0;
 
     public static string Solve()
@@ -18,35 +18,31 @@ public static class Day6
         {
             if (line.Contains('^'))
             {
-                position = (map.Count, line.IndexOf('^'));
+                position = (map.Count + 1, line.IndexOf('^') + 1);
             }
 
-            map.Add([.. line]);
-            visited.Add(Enumerable.Repeat(false, line.Length).ToList());
+            map.Add([.. ' ' + line + ' ']);
         }
 
-        while (position.Item1 >= 0 && position.Item2 >= 0
-            && position.Item1 < map.Count && position.Item2 < map[0].Count)
+        // Padding
+        map.Insert(0, Enumerable.Repeat(' ', map[0].Count).ToList());
+        map.Add(Enumerable.Repeat(' ', map[0].Count).ToList());
+
+        while (position.Item1 > 0 && position.Item2 > 0
+            && position.Item1 < (map.Count - 1) && position.Item2 < (map[0].Count - 1))
         {
-            try
+            visited.Add((position.Item1, position.Item2));
+            if (map[position.Item1 + Directions[direction].Item1][position.Item2 + Directions[direction].Item2] != '#')
             {
-                if (map[position.Item1 + Directions[direction].Item1][position.Item2 + Directions[direction].Item2] != '#')
-                {
-                    position = (position.Item1 + Directions[direction].Item1, position.Item2 + Directions[direction].Item2);
-                    visited[position.Item1][position.Item2] = true;
-                }
-                else
-                {
-                    direction = (direction + 1) % 4;
-                }
+                position = (position.Item1 + Directions[direction].Item1, position.Item2 + Directions[direction].Item2);
             }
-            catch (ArgumentOutOfRangeException)
+            else
             {
-                break;
+                direction = (direction + 1) % 4;
             }
         }
 
-        return visited.Sum(v => v.Count(e => e)).ToString();
+        return visited.Count().ToString();
     }
 
 
